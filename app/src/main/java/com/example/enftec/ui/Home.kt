ad3f588010a6f89e5ef117a543914ac588a57176
devarthur4718 <Dev4718@face>
2020-03-09 18:07:00 -0300

    home

--- app/src/main/java/com/example/enftec/ui/Home.kt
+++ app/src/main/java/com/example/enftec/ui/Home.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-
 import com.example.enftec.R
 import com.example.enftec.databinding.HomeFragmentBinding
 
