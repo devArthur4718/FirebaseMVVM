4c978c35dd7b8e8b27cfb4c96a48b34ede6946cc
devarthur4718 <Dev4718@face>
2020-03-09 17:59:50 -0300

    first commit

---
+++ app/src/main/java/com/example/enftec/ui/Home.kt
@@ -0,0 +1,45 @@
+package com.example.enftec.ui
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+
+import com.example.enftec.R
+import com.example.enftec.databinding.HomeFragmentBinding
+
+class Home : Fragment() {
+
+
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var binding: HomeFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.home_fragment,
+            container,
+            false
+        )
+
+        setObservables()
+        return binding.root
+    }
+
+    private fun setObservables() {
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+
+    }
+
+}
